a3d9a179414eb42ce54642fda1da51f4a5e3dc74
Rafael Moreira Xavier <rafaelmoreiraxavier09@gmail.com>
2026-09-17 20:06:09 -0300

    Add exercicio 1

---
+++ relatorio_05/exericio1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int combinar_equipes(int n){
+    if(n==0){
+        return 0;
+    }
+    else if(n==1){
+        return 1;
+    }
+    else if(n>1){
+        return combinar_equipes(n - 1) + combinar_equipes(n - 2);
+    }
+}
+
+int main() 
+{
+    int n, total;
+    cout << "Digite o tamanho do chaveamento (n): "<<endl;
+    cin>>n;
+    total=combinar_equipes(n);
+    cout<< "Total de cenários de confrontos possíveis: "<<total<<endl;
+    return 0;
+}
